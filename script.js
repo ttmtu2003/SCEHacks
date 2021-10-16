d08f5b21940c1a024d379efc6939ca1c7cda07fa
Tu Tran <tutran@Huans-MacBook-Pro.local>
2021-10-16 02:22:25 -0700

    adding function for crises buttons

--- script.js
+++ script.js
@@ -1,3 +1,5 @@
+const overviewTitle = document.querySelector(".crises-title");
+
 //drop down nav
 function myFunction() {
     var x = document.getElementById("nav");
@@ -8,3 +10,15 @@ function myFunction() {
     }
   }
 
+ //crises buttons
+ overviewTitle.addEventListener("click",function(x){
+    if(x.target.classList.contains("crises-button")){
+        const Target = x.target.getAttribute("data-title");
+        overviewTitle.querySelector(".active").classList.remove("active");
+        x.target.classList.add("active");
+
+        const menuItem = document.querySelector(".crises");
+        menuItem.querySelector(".menu-item-content.active").classList.remove("active");
+        menuItem.querySelector(Target).classList.add("active");
+    }
+ })
